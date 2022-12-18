efcfbc964f9239c9cbc11e85bdffc7a9e4846030
groupguanfang <1203970284@qq.com>
2022-12-18 15:45:30 +0800

    add pic

--- src/post.ts
+++ src/post.ts
@@ -1,11 +1,11 @@
-import request from "utils/request"
+import request from "./utils/request"
 import type { TypeGetPost } from "./types/post"
 /**
  * 获取文章列表
  *
- * @param limit 限制
- * @param offset 偏移
- * @param order 排序方式
+ * @param limit 限制获取多少篇文章
+ * @param offset 偏移，这个自行百度用法，长篇大论就不合适了
+ * @param order 排序方式，参考API文档
  */
 export async function GetPost(
   limit?: number,
