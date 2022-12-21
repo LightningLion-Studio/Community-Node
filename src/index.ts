12dd87619b61080279d0d160bbdc1860ef61515d
祥和农业服务器 <1203970284@qq.com>
2022-12-21 20:56:19 +0800

    add export

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,19 @@ export * from "./user"
 export * from "./follow"
 export * from "./comment"
 export * from "./category"
+export * from "./tag"
 
 import * as Post from "./post"
 import * as User from "./user"
 import * as Follow from "./follow"
 import * as Comment from "./comment"
 import * as Category from "./category"
+import * as Tag from "./tag"
 export default {
   ...Post,
   ...User,
   ...Follow,
   ...Comment,
-  ...Category
+  ...Category,
+  ...Tag,
 }
